acb0ee50920f83d8eea4ac68e1fd58fc66a586a8
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-20 16:44:46 +0200

    salygos

--- index.php
+++ index.php
@@ -3,7 +3,106 @@
 
 <?php
 echo '<br>';
-echo 'Salyga: <br>';
+echo 'Parašykite funkciją, kurios argumentas būtų tekstas, kuris yra įterpiamas į h1 tagą. <br>';
+
+
+?>
+
+
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 2 ****** </h3>
+<?php
+echo '<br>';
+echo 'Parašykite funkciją su dviem argumentais, pirmas argumentas tekstas, įterpiamas į h tagą, 
+<br> o antrasis tago numeris (1-6). <br>
+Rašydami šią funkciją remkitės pirmame uždavinyje parašytą funkciją. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 3 ****** </h3>
+<?php
+echo '<br>';
+echo 'Generuokite atsitiktinį stringą, pasinaudodami kodu md5(time()). <br>
+Visus skaitmenis stringe įdėkite į h1 tagą. <br>
+Jegu iš eilės eina keli skaitmenys, juos į tagą reikią dėti kartu <br>
+(h1 atsidaro prieš pirmą ir užsidaro po paskutinio) <br>
+Keitimui naudokite pirmo uždavinio funkciją ir preg_replace_callback(). <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 4 ****** </h3>
+<?php
+echo '<br>';
+echo 'Parašykite funkciją, kuri skaičiuotų, <br> iš kiek sveikų skaičių jos argumentas dalijasi be liekanos <br> 
+(išskyrus vienetą ir patį save) <br> 
+Argumentą užrašykite taip, kad būtų galima įvesti tik sveiką skaičių. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 5 ****** </h3>
+<?php
+echo '<br>';
+echo 'Sugeneruokite masyvą iš 100 elementų, <br> 
+kurio reikšmės atsitiktiniai skaičiai nuo 33 iki 77. <br>
+Išrūšiuokite masyvą pagal daliklių be liekanos kiekį mažėjimo tvarka,<br>
+panaudodami ketvirto uždavinio funkciją. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 6 ****** </h3>
+<?php
+echo '<br>';
+echo 'Sugeneruokite masyvą iš 100 elementų, <br> 
+kurio reikšmės atsitiktiniai skaičiai nuo 333 iki 777. <br> 
+Naudodami 4 uždavinio funkciją iš masyvo ištrinkite pirminius skaičius. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 7 ****** </h3>
+<?php
+echo '<br>';
+echo 'Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą,<br>
+ kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, <br> 
+ o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. <br> 
+ Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. <br>
+ Paskutinio masyvo paskutinis elementas yra lygus 0.<br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 8 ****** </h3>
+<?php
+echo '<br>';
+echo 'Suskaičiuokite septinto uždavinio elementų, kurie nėra masyvai, sumą. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 9 ****** </h3>
+<?php
+echo '<br>';
+echo 'Sugeneruokite masyvą iš trijų elementų,  <br> kurie yra atsitiktiniai skaičiai nuo 1 iki 33.  <br> 
+Jeigu tarp trijų paskutinių elementų yra nors vienas ne pirminis skaičius,  <br>
+prie masyvo pridėkite dar vieną elementą- atsitiktinį skaičių nuo 1 iki 33.  <br>
+Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą.  <br> 
+Kartokite, kol sąlyga nereikalaus pridėti elemento. <br>';
+
+
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 10 ****** </h3>
+<?php
+echo '<br>';
+echo 'Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų,  <br> 
+kurie yra atsitiktiniai skaičiai nuo 1 iki 100.  <br>  
+Jeigu tokio masyvo pirminių skaičių vidurkis mažesnis už 70,  <br>  
+suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3.  <br> 
+Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite.  <br>';
 
 
 ?>
