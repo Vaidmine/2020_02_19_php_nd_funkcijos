a36dbf9d0d052b338e218f67e5f59a6346310607
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-20 16:35:41 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<h2 style="color:rgb(0, 200, 255); padding-left:80px;">***** FUNKCIJOS ****** </h2>
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 1 ****** </h3>
+
+<?php
+echo '<br>';
+echo 'Salyga: <br>';
+
+
+?>
