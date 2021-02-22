b87107a501c0d300f5060b82b4ec396b713f5a3d
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-22 18:35:04 +0200

    funkcijos

--- index.php
+++ index.php
@@ -3,20 +3,34 @@
 
 <?php
 echo '<br>';
-echo 'Parašykite funkciją, kurios argumentas būtų tekstas, kuris yra įterpiamas į h1 tagą. <br>';
+echo 'Parašykite funkciją, kurios argumentas būtų tekstas, kuris yra įterpiamas į h1 tagą. <br><br>';
 
+$text = 'Lorem ipsum is a dummy text used in print, graphic or web designs.';
+function tekstas($text)
+{
+    return "<h1>$text</h1>";
+};
+echo tekstas($text);
 
 ?>
-
 
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 2 ****** </h3>
 <?php
 echo '<br>';
-echo 'Parašykite funkciją su dviem argumentais, pirmas argumentas tekstas, įterpiamas į h tagą, 
-<br> o antrasis tago numeris (1-6). <br>
-Rašydami šią funkciją remkitės pirmame uždavinyje parašytą funkciją. <br>';
+echo 'Parašykite funkciją su dviem argumentais, pirmas argumentas tekstas, įterpiamas į h tagą, <br>
+o antrasis tago numeris (1-6). <br>
+Rašydami šią funkciją remkitės pirmame uždavinyje parašytą funkciją. <br><br>';
 
+$text2 = 'Some Lorem Ipsum random size.';
+$hTagNum = rand(1, 6);
+echo "H Tag number is random, right now it is: $hTagNum";
+
+function tekstas2($text2, $hTagNum)
+{
+    return "<h$hTagNum>$text2</h$hTagNum>";
+};
+echo tekstas2($text2, $hTagNum);
 
 ?>
 
@@ -27,39 +41,114 @@ echo 'Generuokite atsitiktinį stringą, pasinaudodami kodu md5(time()). <br>
 Visus skaitmenis stringe įdėkite į h1 tagą. <br>
 Jegu iš eilės eina keli skaitmenys, juos į tagą reikią dėti kartu <br>
 (h1 atsidaro prieš pirmą ir užsidaro po paskutinio) <br>
-Keitimui naudokite pirmo uždavinio funkciją ir preg_replace_callback(). <br>';
+Keitimui naudokite pirmo uždavinio funkciją ir preg_replace_callback(). <br><br>';
 
+echo 'MD5 hash - taking a string of an any length and encoding it into a 128-bit hash output. Sample: <br>';
+echo 'preg_replace_callback — Perform a regular expression search and replace using a callback';
 
+$randomString = md5(time());
+echo "<br><br><b>Sugeneruojame atsitiktinį 'hash' numerį:</b><br>";
+echo $randomString . '<br><br>';
+
+$numText = preg_replace_callback('/\D+/', "letterRemoval", $randomString);
+$finalText = preg_replace_callback('/\d+/', "letterShift", $numText);
+
+function letterRemoval($matches){
+    return ' '; 
+}
+
+function tagGenerating($text){
+    return $text;
+}
+
+function letterShift($matches) {
+    return '<h1>'.$matches[0].'</h1>';
+}
+
+echo tagGenerating($finalText);
+ 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 4 ****** </h3>
 <?php
 echo '<br>';
-echo 'Parašykite funkciją, kuri skaičiuotų, <br> iš kiek sveikų skaičių jos argumentas dalijasi be liekanos <br> 
-(išskyrus vienetą ir patį save) <br> 
+echo 'Parašykite funkciją, kuri skaičiuotų,  iš kiek sveikų skaičių jos argumentas dalijasi be liekanos <br>
+(išskyrus vienetą ir patį save) <br>
 Argumentą užrašykite taip, kad būtų galima įvesti tik sveiką skaičių. <br>';
 
+function pirminiaiSkaiciai(int $number)
+{
+    global $dividers; // importavimui del syntax
+
+    for ($i = 2; $i < $number; $i++) { // 2 nes negali dalintis is 1 ir pacio saves
+        if ($number % $i == 0) {
+            $dividers++;
+            //    echo $i. '<br>'; // dalikliai is kuriu dalinasi skaicius be liekanos;
+        }
+    }
+    return $dividers;
+}
+
+echo "<br><b>  Sveiki skaičiai, kurie dalinasi iš funkcijoje įrašyto skaičiaus (kurio čia nesimato):</b><br>";
+echo pirminiaiSkaiciai(60);
+echo "<br> <br>";
+// echo $dividers;
 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 5 ****** </h3>
 <?php
 echo '<br>';
-echo 'Sugeneruokite masyvą iš 100 elementų, <br> 
+echo 'Sugeneruokite masyvą iš 100 elementų, <br>
 kurio reikšmės atsitiktiniai skaičiai nuo 33 iki 77. <br>
 Išrūšiuokite masyvą pagal daliklių be liekanos kiekį mažėjimo tvarka,<br>
 panaudodami ketvirto uždavinio funkciją. <br>';
 
+$array = [];
+
+foreach (range(1, 10) as $_) {
+    $array[] = rand(33, 77);
+}
+
+// print_r($array);
+usort($array, function ($a, $b) {
+    return pirminiaiSkaiciai($b) <=> pirminiaiSkaiciai($a);
+});
+
+echo '<pre>';
+print_r($array);
+echo '</pre>';
 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 6 ****** </h3>
 <?php
 echo '<br>';
-echo 'Sugeneruokite masyvą iš 100 elementų, <br> 
-kurio reikšmės atsitiktiniai skaičiai nuo 333 iki 777. <br> 
+echo 'Sugeneruokite masyvą iš 100 elementų, <br>
+kurio reikšmės atsitiktiniai skaičiai nuo 333 iki 777. <br>
 Naudodami 4 uždavinio funkciją iš masyvo ištrinkite pirminius skaičius. <br>';
 
+$array6 = [];
+
+foreach (range(1, 10) as $_) {
+    $array6[] = rand(333, 777);
+}
+echo '<pre>';
+echo 'masyvas pirminis <br>';
+print_r($array6);
+echo '</pre>';
+
+$ilgis = count($array6);
+
+for ($i = 0; $i < $ilgis; $i++) {
+    if (pirminiaiSkaiciai($array6[$i]) == 0) {
+        unset($array6[$i]); //istrina viska
+    }
+}
+echo '<pre>';
+echo 'masyvas po istrynimo <br>';
+print_r($array6);
+echo '</pre>';
 
 ?>
 
@@ -67,11 +156,35 @@ Naudodami 4 uždavinio funkciją iš masyvo ištrinkite pirminius skaičius. <br
 <?php
 echo '<br>';
 echo 'Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą,<br>
- kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, <br> 
- o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. <br> 
+ kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, <br>
+ o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. <br>
  Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. <br>
  Paskutinio masyvo paskutinis elementas yra lygus 0.<br>';
 
+ $rand = rand(10, 30);
+
+function generateFunction($rand) {
+    $num1 = rand(10, 20);
+    for ($i = 0; $i < $num1; $i++) {
+        if ($i < $num1 - 1) {
+            $array7[$i] = rand(10, 20);
+        } else {
+            if($rand > 0) {
+                    $array7[$i] = generateFunction($rand - 1);
+            } else {
+                $array7[] = 0;
+            }
+        }
+    }
+return $array7; 
+}
+
+
+echo '<pre>';
+echo '<br>';
+print_r (generateFunction($rand));
+// print_r($array7);
+echo '</pre>';
 
 ?>
 
@@ -80,29 +193,63 @@ echo 'Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą,<br>
 echo '<br>';
 echo 'Suskaičiuokite septinto uždavinio elementų, kurie nėra masyvai, sumą. <br>';
 
+// function suma7(){
+// $sum = 0;
+// foreach ($array7 as $key => $value) {
+//     # code...
+// }
+
+// }
+
+// echo '<pre>';
+// echo '<br>';
+// print_r (array_sum($array7[$i]));
+
+// echo '</pre>';
 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 9 ****** </h3>
 <?php
 echo '<br>';
-echo 'Sugeneruokite masyvą iš trijų elementų,  <br> kurie yra atsitiktiniai skaičiai nuo 1 iki 33.  <br> 
+echo 'Sugeneruokite masyvą iš trijų elementų,  
+<br> kurie yra atsitiktiniai skaičiai nuo 1 iki 33.  <br>
 Jeigu tarp trijų paskutinių elementų yra nors vienas ne pirminis skaičius,  <br>
 prie masyvo pridėkite dar vieną elementą- atsitiktinį skaičių nuo 1 iki 33.  <br>
-Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą.  <br> 
+Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą.  <br>
 Kartokite, kol sąlyga nereikalaus pridėti elemento. <br>';
 
+function isPrime($num){
+    if(0 ==$num || 1==$num) return false;
+    if (2 == $num) return true;
+    for ($i=2; $i < $num ; $i++) { 
+        if ($num % $i ==0) return false; 
+        }
+        return true;
+    }
+
+for ($i=0; $i < 3 ; $i++) { 
+    $array9[$i] = rand(1, 33);
+}
+
+    while (isPrime($array9[count($array9)-1]) || isPrime($array9[count($array9)-2]) || isPrime($array9[count($array9)-3]) ) {
+        $array9[] = rand(1, 33);
+}
+
+echo '<pre>';
+echo '<br>';
+print_r ($array9);
+echo '</pre>';
 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 10 ****** </h3>
 <?php
 echo '<br>';
-echo 'Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų,  <br> 
-kurie yra atsitiktiniai skaičiai nuo 1 iki 100.  <br>  
-Jeigu tokio masyvo pirminių skaičių vidurkis mažesnis už 70,  <br>  
-suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3.  <br> 
+echo 'Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų,  <br>
+kurie yra atsitiktiniai skaičiai nuo 1 iki 100.  <br>
+Jeigu tokio masyvo pirminių skaičių vidurkis mažesnis už 70,  <br>
+suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3.  <br>
 Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite.  <br>';
-
 
 ?>
